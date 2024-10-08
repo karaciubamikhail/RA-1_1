4d1df8361e6e43a237ad3610cb88af111b349d47
Mikhail <134963272+karaciubamikhail@users.noreply.github.com>
2024-10-08 17:11:02 +0300

    Done

---
+++ src/Storefunc.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+
+function storefunc ({brand, title, description, descriptionFull, price, currency}){
+    return (
+        <div className="main-content">
+            <h2>{brand}</h2>
+            <h1>{title}</h1>
+            <h3>{description}</h3>
+            <div className="description">
+                {descriptionFull}
+            </div>
+            <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+            <div className="divider"></div>
+            <div className="purchase-info">
+                <div className="price">{price}{currency}</div>
+            <button>Добавить в корзину</button>
+            </div>
+        </div>
+    )
+}
+export default storefunc;
